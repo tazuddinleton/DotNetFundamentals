38d4c6351300a3c3783b0ce28e8cce9c17bb8db1
Taz Uddin <getleton@gmail.com>
2019-09-30 21:04:03 +0600

    LINQ

---
+++ EFCore/Infrastructure/DataContext.cs
@@ -0,0 +1,22 @@
+﻿using EFCore.Entities;
+using JetBrains.Annotations;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFCore.Infrastructure
+{
+    public class DataContext : DbContext
+    {
+        public DataContext()
+        {
+
+        }
+
+        public DbSet<Person> People { get; }
+    }
+}
---
+++ EFCore/Program.cs
@@ -0,0 +1,24 @@
+﻿using EFCore.Entities;
+using EFCore.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            DataContext dataContext = new DataContext();
+
+            dataContext.People.Add(new Person());
+
+
+
+        }
+    }
+}
